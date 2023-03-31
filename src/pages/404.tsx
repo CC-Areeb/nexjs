a9177e5785a9674dd3247998942c6e394c782681
areeb <areeb.malik@cooperativecomputing.com>
2023-03-31 10:25:39 +0500

    Custom 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+const NotFound = () => {
+    return (
+        <>
+            <div className="not-found text-center mt-4">
+                <h1 className="display-4">Oooops...</h1>
+                <h4 className="display-5">The page cannot be found.</h4>
+                <p>
+                    Back to home 👉<Link href="/" className="text-decoration-none text-success fs-5">Homepage</Link>
+                </p>
+            </div>
+        </>
+    );
+}
+
+export default NotFound;
